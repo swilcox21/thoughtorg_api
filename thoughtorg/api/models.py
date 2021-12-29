0fc2864255636e834ead5a11c904d8748c1854eb
Sam Wilcox <swilcox21@gmail.com>
2021-12-29 10:35:39 -0500

    manytoone

--- thoughtorg/api/models.py
+++ thoughtorg/api/models.py
@@ -2,16 +2,15 @@ from django.db import models
 
 # Create your models here.
 
-# class Folder(models.Model):
-#     label = models.CharField(max_length=60)
+class Folder(models.Model):
+    label = models.CharField(max_length=60)
 
-#     def __str__(self):
-#         return self.content
+    def __str__(self):
+        return self.label
 
 class Thought(models.Model):
     content = models.CharField(max_length=2000)
-    folder = models.CharField(max_length=60)
+    folder = models.ForeignKey(Folder, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.content
-
